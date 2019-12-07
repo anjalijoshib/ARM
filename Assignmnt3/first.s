d300d9b9cba512ed8733fc35c880d85d4d91c2d1
anjalijoshib <42800435+anjalijoshib@users.noreply.github.com>
2019-12-07 12:33:50 +0530

    with print truth tble code

--- Assignmnt3/first.s
+++ Assignmnt3/first.s
@@ -1,4 +1,4 @@
- ;logic gate implementation using neural network
+;logic gate implementation using neural network
  
  ; format for switch case statement in arm assembly
 ; ADR.W R0, BranchTable_Byte
@@ -19,7 +19,11 @@
      THUMB
      AREA     appcode, CODE, READONLY
      EXPORT __main
-	 IMPORT printMsg
+	 IMPORT printMsgAND
+	 IMPORT printMsgOR
+	 IMPORT printMsgNOT
+	 IMPORT printMsgNAND
+	 IMPORT printMsgNOR
      IMPORT printMsg4p
 	 ENTRY 
 __main  FUNCTION
@@ -27,17 +31,18 @@ __main  FUNCTION
 	    ;for LOGIC_NOT following are the valid combination of input as can be seen in python code 
 		;X0->1, X1->0, X2->0 ; we are considering third input X2(deactive) to keep similarity for all logic operations
 		;X0->1, X1->1, X2->0
-		VLDR.F32 S29,=1 ;X0
-		VLDR.F32 S30,=1	;X1
-		VLDR.F32 S31,=0	;X2
+		
+;		VLDR.F32 S29,=1 ;X0
+;		VLDR.F32 S30,=1	;X1
+;		VLDR.F32 S31,=0	;X2
 		
 		;to display it we have to store input in registers
-		MOV R0,#1
-		MOV R1,#1
-		MOV R2,#0
-		ADR.W  R6, BranchTable_Byte ; generate register relative address accorfing to Branchtable_Byte lable
+;		MOV R0,#1
+;		MOV R1,#1
+;		MOV R2,#0
+;		ADR.W  R6, BranchTable_Byte ; generate register relative address accorfing to Branchtable_Byte lable
 		
-		MOV R7,#0 ; to select one option in switch case (gates)
+		;MOV R7,#0 ; to select one option in switch case (gates)
 		;0->LOGIC_AND
 		;1->LOGIC_OR
 		;2->LOGIC_NOT
@@ -45,92 +50,158 @@ __main  FUNCTION
 		;4->LOGIC_NOR
 		;5->LOGIC_XOR
 		;6->LOGIC_XNOR
-		TBB   [R6, R7] ; switch case equivalent in Arm cortex M4 so it will save table with length equivlent to BranchTable_Byte and R holds the table pointer
+		;TBB   [R6, R7] ; switch case equivalent in Arm cortex M4 so it will save table with length equivlent to BranchTable_Byte and R holds the table pointer
 		
 		;S0 = W0,S1 = W1, S2 = W2, S3 = (Bias) as provided in the python code
-LOGIC_AND	VLDR.F32 S0,=-0.1
+
+LOGIC_AND	BL printMsgAND
+			VLDR.F32 S0,=-0.1
 			VLDR.F32 S1,=0.2
 			VLDR.F32 S2,=0.2
 			VLDR.F32 S3,=-0.2
-			B EXP_X_CALCULATION
-LOGIC_OR	VLDR.F32 S0,=-0.1
+			B INPUTS_1
+
+LOGIC_OR	BL printMsgOR
+			VLDR.F32 S0,=-0.1
 			VLDR.F32 S1,=0.7
 			VLDR.F32 S2,=0.7
 			VLDR.F32 S3,=-0.1
-			B EXP_X_CALCULATION
-LOGIC_NOT	VLDR.F32 S0,=0.5
+			B INPUTS_1
+
+LOGIC_NOT	BL printMsgNOT
+			VLDR.F32 S0,=0.5
 			VLDR.F32 S1,=-0.7
 			VLDR.F32 S2,=0
 			VLDR.F32 S3,=0.1
-			B EXP_X_CALCULATION
-LOGIC_NAND	VLDR.F32 S0,=0.6
+			B INPUTS_1
+
+LOGIC_NAND	BL printMsgNAND
+			VLDR.F32 S0,=0.6
 			VLDR.F32 S1,=-0.8
 			VLDR.F32 S2,=-0.8
 			VLDR.F32 S3,=0.3
-			B EXP_X_CALCULATION
-LOGIC_NOR	VLDR.F32 S0,=0.5
+			B INPUTS_1
+
+LOGIC_NOR	BL printMsgNOR
+			VLDR.F32 S0,=0.5
 			VLDR.F32 S1,=-0.7
 			VLDR.F32 S2,=-0.7
 			VLDR.F32 S3,=0.1
-			B EXP_X_CALCULATION
-LOGIC_XOR	VLDR.F32 S0,=-5
-			VLDR.F32 S1,=20
-			VLDR.F32 S2,=10
-			VLDR.F32 S3,=1
-			B EXP_X_CALCULATION
-LOGIC_XNOR	VLDR.F32 S0,=-5
-			VLDR.F32 S1,=20
-			VLDR.F32 S2,=10
-			VLDR.F32 S3,=1
+			B INPUTS_1
+
+INPUTS_1	MOV R0,#1; X1
+			VMOV.F32 S29,R0 ; shifting input to floating point register
+			VCVT.F32.S32 S29,S29; converting input to signed floating point register
+			MOV R1,#0; X2
+			VMOV.F32 S30,R1 ; shifting input to floating point register
+			VCVT.F32.S32 S30,S30; converting input to signed floating point register
+			MOV R2,#0; X3
+			VMOV.F32 S31,R2 ; shifting input to floating point register
+			VCVT.F32.S32 S31,S31; converting input to signed floating point register
 			B EXP_X_CALCULATION
 
+INPUTS_2	MOV R0,#1; X1
+			VMOV.F32 S29,R0 ; shifting input to floating point register
+			VCVT.F32.S32 S29,S29; converting input to signed floating point register
+			MOV R1,#0; X2
+			VMOV.F32 S30,R1 ; shifting input to floating point register
+			VCVT.F32.S32 S30,S30; converting input to signed floating point register
+			MOV R2,#1; X3
+			VMOV.F32 S31,R2 ; shifting input to floating point register
+			VCVT.F32.S32 S31,S31; converting input to signed floating point register
+			B EXP_X_CALCULATION
+
+INPUTS_3	MOV R0,#1; X1
+			VMOV.F32 S29,R0 ; shifting input to floating point register
+			VCVT.F32.S32 S29,S29; converting input to signed floating point register
+			MOV R1,#1; X2
+			VMOV.F32 S30,R1 ; shifting input to floating point register
+			VCVT.F32.S32 S30,S30; converting input to signed floating point register
+			MOV R2,#0; X3
+			VMOV.F32 S31,R2 ; shifting input to floating point register
+			VCVT.F32.S32 S31,S31; converting input to signed floating point register
+			B EXP_X_CALCULATION
+
+INPUTS_4	MOV R0,#1; X1
+			VMOV.F32 S29,R0 ; shifting input to floating point register
+			VCVT.F32.S32 S29,S29; converting input to signed floating point register
+			MOV R1,#1; X2
+			VMOV.F32 S30,R1 ; shifting input to floating point register
+			VCVT.F32.S32 S30,S30; converting input to signed floating point register
+			MOV R2,#1; X3
+			VMOV.F32 S31,R2 ; shifting input to floating point register
+			VCVT.F32.S32 S31,S31; converting input to signed floating point register
+			B EXP_X_CALCULATION
+
+
 ;S28 will store the final X0*W0 + X1*W1 + X2*W2 + Bias
-EXP_X_CALCULATION	VMLA.F32 S28, S0, S29
-					VMLA.F32 S28, S1, S30
-					VMLA.F32 S28, S2, S31
+EXP_X_CALCULATION	VMUL.F32 S16, S0, S29
+					VMOV.F32 S28, S16
+					VMUL.F32 S17, S1, S30
+					VADD.F32 S28, S28, S17
+					VMUL.F32 S18, S2, S31
+					VADD.F32 S28, S28, S18
 					VADD.F32 S28, S28, S3
 					B EXPROUTINE
 
-;offset calculation for switch case
-BranchTable_Byte	DCB    0
-					DCB    ((LOGIC_OR-LOGIC_AND)/2)
-					DCB    ((LOGIC_NOT-LOGIC_AND)/2)
-					DCB    ((LOGIC_NAND-LOGIC_AND)/2)
-					DCB    ((LOGIC_NOR-LOGIC_AND)/2)
-					DCB    ((LOGIC_XOR-LOGIC_AND)/2)
-					DCB    ((LOGIC_XNOR-LOGIC_AND)/2)
 
 ;this program performs e^x,the result will be stored in S2
-EXPROUTINE	VMOV.F32 S2, #1 ; Sum Variable
-			VMOV.F32 S6, #25 ; 'n' variable - sequence size
-			;VMOV.F32 S1, #5 ; 'x' varaiable - in e^x
-			VMOV.F32 S3, #1 ; constant
-loop		VCMP.F32 S6, #0
-			VMRS.F32 APSR_nzcv,FPSCR ; Transfer floating-point flags to the APSR flags
-			BEQ SIGROUTINE
-			VDIV.F32 S4, S2, S6 ; sum/i
-			;VMUL.F32 S5, S1, S4 ; x*sum/i
-			VMUL.F32 S5, S28, S4 ; x*sum/i
-			VADD.F32 S2, S3, S5 ; sum = 1 + (x * (sum/i))
-			VSUB.F32 S6, S6, S3
-			B loop
-SIGROUTINE	VDIV.F32 S2, S3, S2 ; 1/e^x
-			VADD.F32 S2, S3, S2 ; 1 + 1/e^x
-			VDIV.F32 S2, S3, S2 ; 1/(1 + 1/e^x)
-			B OUTPUT
-;SIGROUTINE
-			;VMOV.F32 S7, S2 ; e^x to S7
-			;VADD.F32 S8, S3, S7 ; 1 + e^x to S8
-			;VDIV.F32 S9, S7, S8
-			;B OUTPUT
-; S15 will hold 0.5 for comparison to finalise the logical output for a particular gate
-OUTPUT	VLDR.F32 S15 ,=0.5 ; thresholding to determine if logic 1 or logic 0
-		VCMP.F32 S2, S15 ; compare the output of sigmoid routine with S15
-		VMRS.F32 APSR_nzcv,FPSCR ; Transfer floating-point flags to the APSR flags
-		ITE HI
-		MOVHI R3,#1; if S2 > S15
-		MOVLS R3,#0; if S2 < S15
+
+EXPROUTINE	VMOV.F32 S5, S28; x:Number to find e^x
+	        VMOV.F32 S6, #30; Number of iterations for e^x expansion
+			VMOV.F32 S7, #1;  count
+			VMOV.F32 S8, #1;  temp variable
+			VMOV.F32 S9, #1;  result initialized to 1
+			VMOV.F32 S11, #1;  register to hold 1
+			VMOV.F32 S14,#1;
+
+Loop 		VCMP.F32 S6, S7; Comparison done for excuting taylor series expansion of e^x for s2 number of terms
+			VMRS.F32 APSR_nzcv,FPSCR; to copy fpscr to apsr
+			BLT SIGROUTINE;
+			VDIV.F32 S10, S5, S7; temp1=x/count
+			VMUL.F32 S8, S8, S10; temp=temp*temp1;
+			VADD.F32 S9, S9, S8; result=result+temp;
+			VADD.F32 S7, S7, S11; count++
+			B Loop;
+			
+SIGROUTINE	 	VADD.F32 S12,S9,S14;  (1+e^X)
+			VDIV.F32 S13,S9,S12;	  g(X) = 1/(1+e^-X) == (e^X)/(1+e^X)
+			B OUT;
+	 
+OUT 	VLDR.F32 S15,=0.5
+		VCMP.F32 S13,S15
+		VMRS.F32 APSR_nzcv,FPSCR;Used to copy fpscr to apsr
+		ITE HI		
+		MOVHI R3,#1; if S13 > S15
+		MOVLS R3,#0; if S13 < S15
 		BL printMsg4p	 ; Refer to ARM Procedure calling standards.
+		
+		;Iterations for further three sets of inputs
+	ADD R4, R4,#1
+	CMP R4,#1
+	BEQ INPUTS_2
+	CMP R4,#2
+	BEQ INPUTS_3
+	CMP R4,#3
+	BEQ INPUTS_4
+	MOV R4,#0
+	
+	ADD R5, R5,#1
+	
+	;Iterations for further LOGIC GATES
+	CMP R5,#1	;Go to logic OR 
+	BEQ LOGIC_OR
+
+	CMP R5,#2	;Go to logic NOT 
+	BEQ LOGIC_NOT
+
+	CMP R5,#3	;Go to logic NAND 
+	BEQ LOGIC_NAND
+
+	CMP R5,#4	;Go to logic NOR 
+	BEQ LOGIC_NOR
+
+		
 	 
 stop B stop ; stop program
 	 ENDFUNC
--- Assignmnt3/printMsg.c
+++ Assignmnt3/printMsg.c
@@ -1,17 +1,80 @@
-
 #include "stm32f4xx.h"
 #include <string.h>
-void printMsg(const int a)
+void printMsgAND(const int a)
 {
 	 char Msg[100];
 	 char *ptr;
-	 sprintf(Msg, "%x", a);
+	 sprintf(Msg,"\n LOGIC AND--------------------------");
 	 ptr = Msg ;
-   while(*ptr != '\0'){
+   while(*ptr != '\0')
+	 {
       ITM_SendChar(*ptr);
       ++ptr;
    }
-}
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   }
+
+	 void printMsgOR(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg,"\n LOGIC OR---------------------------");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   }
+
+void printMsgNOT(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg,"\n LOGIC NOT--------------------------");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   }	
+
+void printMsgNAND(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg,"\n LOGIC NAND-------------------------");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   }	 
+
+void printMsgNOR(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg,"\n LOGIC NOR--------------------------");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   }	 
+/*
 void printMsg2p(const int a, const int b)
 {
 	 char Msg[100];
@@ -30,21 +93,20 @@ void printMsg2p(const int a, const int b)
       ITM_SendChar(*ptr);
       ++ptr;
    }
-}
+}*/
 
 void printMsg4p(const int a, const int b, const int c, const int d)
 {
 	 char Msg[100];
 	 char *ptr;
-	 // Printing the message
-	sprintf(Msg,"Truth Table \n X1:  ");
+	 // Printing the first input X1
+	sprintf(Msg,"\n X1:");
 	 ptr = Msg ;
    while(*ptr != '\0')
 	 {
       ITM_SendChar(*ptr);
       ++ptr;
    }
-	 //Printing the first parameter
 	 sprintf(Msg, "%x", a);
 	 ptr = Msg ;
    while(*ptr != '\0')
@@ -52,8 +114,8 @@ void printMsg4p(const int a, const int b, const int c, const int d)
       ITM_SendChar(*ptr);
       ++ptr;
    }
-	 // Printing the message
-	 sprintf(Msg,"\t X2: ");
+	 // Printing the second input X2
+	 sprintf(Msg,"\t X2:");
 	 ptr = Msg ;
    while(*ptr != '\0')
 	 {
@@ -67,8 +129,8 @@ void printMsg4p(const int a, const int b, const int c, const int d)
       ITM_SendChar(*ptr);
       ++ptr;
    }
-	 // Printing the message
-	 sprintf(Msg,"\t X3: ");
+	 // Printing the third input X3
+	 sprintf(Msg,"\t X3:");
 	 ptr = Msg ;
    while(*ptr != '\0')
 	 {
@@ -82,8 +144,8 @@ void printMsg4p(const int a, const int b, const int c, const int d)
       ITM_SendChar(*ptr);
       ++ptr;
    }
-	 // Printing the message
-	 sprintf(Msg,"\t OUTPUT: ");
+	 // Printing the OUTPUT Y
+	 sprintf(Msg,"\t OUT Y:");
 	 ptr = Msg ;
    while(*ptr != '\0')
 	 {
@@ -97,7 +159,5 @@ void printMsg4p(const int a, const int b, const int c, const int d)
       ITM_SendChar(*ptr);
       ++ptr;
 	 }
-
 	
 }
-
